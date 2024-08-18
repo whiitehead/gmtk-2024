10273c73f8d3c7f827ad0bc7e7db7aff465d6ebb
Caleb Whitehead <whiitehead@gmail.com>
2024-08-18 11:02:38 -0700

    Add rigid body to level and make it a prefab

--- Assets/Scripts/RotationRemover.cs
+++ Assets/Scripts/RotationRemover.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class RotationRemover : MonoBehaviour
 {
-    void Update() => transform.rotation = Quaternion.identity;
+    private void Update() => transform.rotation = Quaternion.identity;
 }
